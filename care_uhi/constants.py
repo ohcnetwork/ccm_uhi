2fe85500513e9f7d38bed846ae99ac1d6662aceb
nandkishorr <rachulr7@gmail.com>
2026-05-05 10:35:55 +0530

    added mappers

---
+++ care_uhi/constants.py
@@ -0,0 +1,16 @@
+"""
+CARE platform constants for Beckn/UHI protocol responses.
+
+Catalog descriptor = CARE platform (org-level HSPA identity).
+Providers = Individual facilities/hospitals within CARE.
+"""
+
+CARE_CATALOG_DESCRIPTOR = {
+    "name": "CARE - Open Health Care Network",
+    "short_desc": "Open-source health care platform by Open Health Care Network (OHC)",
+    "images": [
+        {
+            "url": "https://care.ohc.network/images/care-logo.png",
+        }
+    ],
+}
